f7701dd54d2b1d49ad1f54f9911ac25afd23c53c
Miroslav Misik <mirom73@outlook.com>
2019-03-10 22:42:40 +0100

    fixed bug, number of printed numbers was 200 not 100 as specified

--- Chapter01/Exercise10/Exercise10.cs
+++ Chapter01/Exercise10/Exercise10.cs
@@ -10,12 +10,12 @@ namespace Chapter01
             Console.WriteLine("EX10: Print the first 100 members of the sequence 2, -3, 4, -5, 6, -7, 8 ...");
             int x = 2;
             int y = -3;
-            Console.Write("{0} {1} ", x, y);
-            for (int i = 1; i < 100; i++)
+            Console.Write("{0}, {1}", x, y);
+            for (int i = 1; i < 50; i++)
             {
                 x += 2;
                 y -= 2;
-                Console.Write("{0} {1} ", x, y);
+                Console.Write(", {0}, {1}", x, y);
             }
             Console.WriteLine();
         }
