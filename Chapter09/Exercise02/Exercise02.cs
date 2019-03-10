212e9eca39544db1fb8df038edde00665c6413eb
Miroslav Misik <mirom73@outlook.com>
2019-03-10 19:09:09 +0100

    completed ch09 ex02, 03

---
+++ Chapter09/Exercise02/Exercise02.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Chapter09
+{
+    internal class Exercise02
+    {
+        static void Main(string[] args)
+        {
+            //2.Create a method GetMax() with two integer(int) parameters, that
+            //returns maximal of the two numbers. Write a program that reads three
+            //numbers from the console and prints the biggest of them. Use the
+            //GetMax() method you just created. Write a test program that validates
+            //that the methods works correctly.
+            Console.WriteLine("EX2: Enter 3 numbers, I will return bigest one.");
+            Console.Write("Enter numer 1: ");
+            int numberOne = int.Parse(Console.ReadLine());
+            Console.Write("Enter numer 2: ");
+            int numberTwo = int.Parse(Console.ReadLine());
+            Console.Write("Enter numer 3: ");
+            int numberThree = int.Parse(Console.ReadLine());
+            int biggestOne = numberOne;
+            biggestOne = GetMax(numberOne, numberTwo);
+            biggestOne = GetMax(biggestOne, numberThree);
+
+            if (biggestOne >= numberOne && biggestOne >= numberTwo && biggestOne >= numberThree)
+            {
+                Console.WriteLine("The biggest number is " + biggestOne);
+            }
+            else
+            {
+                Console.WriteLine("Something is wrong, returned number {0} is not biggest!!!", biggestOne);
+            }
+        }
+
+        static int GetMax(int x, int y)
+        {
+            if (x >= y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+        }
+
+    }
+}
---
+++ Chapter09/Exercise03/Exercise03.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Chapter09
+{
+    internal class Exercise03
+    {
+        static void Main(string[] args)
+        {
+            //3.Write a method that returns the English name of the last digit of a
+            //given number. Example: for 512 prints "two"; for 1024 -> "four".
+            Console.WriteLine("EX3: Method that returns the English name of the last digit.");
+            Console.Write("Enter number: ");
+            string number = Console.ReadLine();
+            Console.WriteLine($"The last entered number in the number {number} is {GetNumberName(int.Parse(number.Substring(number.Length - 1, 1)))}.");
+        }
+
+        static string GetNumberName(int number)
+        {
+            string[] numberNames = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            return numberNames[number];
+        }        
+    }
+}
