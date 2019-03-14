2c75df68e6b108df1ad20f2290f867251aeb3548
Miroslav Misik <mirom73@gmail.com>
2019-03-14 21:44:03 +0100

    finished exercise 4 chapter 7

---
+++ Chapter07/Exercise04/Exercise04.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Chapter07
+{
+    class Exercise04
+    {
+        static void Main(string[] args)
+        {
+            //4. Write a program, which finds the maximal sequence of consecutive 
+            //equal elements in an array. E.g.: { 1, 1, 2, 3, 2, 2, 2, 1} -> { 2, 2, 2}. 
+            Console.WriteLine("EX04: Program, which finds the maximal sequence of consecutive\n" +
+                "equal elements in an array. E.g.: { 1, 1, 2, 3, 2, 2, 2, 1} -> { 2, 2, 2}.");
+            Console.Write("Enter numbers delimited by comma: ");
+            string[] inputIntArray = Console.ReadLine().Split(',');            
+            int endIndex = 0;
+            int maxSequenceLength = 1;
+            int maxSequenceLengthTMP = 1;
+            for (int i = 1; i < inputIntArray.Length; i++)
+            {                
+                if (int.Parse(inputIntArray[i-1]) == int.Parse(inputIntArray[i]))
+                {
+                    maxSequenceLengthTMP += 1;
+                }
+                else
+                {
+                    if (maxSequenceLength < maxSequenceLengthTMP)
+                    {
+                        maxSequenceLength = maxSequenceLengthTMP;
+                        endIndex = i - 1;
+                        maxSequenceLengthTMP = 1;
+                    }
+                }
+            }
+            Console.WriteLine($"length = {maxSequenceLength}, start index = {endIndex - maxSequenceLength + 1}, " +
+                $"end index = {endIndex}");
+            Console.Write(inputIntArray[endIndex - maxSequenceLength + 1]);
+            for (int i = 1; i < maxSequenceLength; i++)
+            {
+                Console.Write($", {inputIntArray[endIndex - maxSequenceLength + 1 + i]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
