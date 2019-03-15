bc3fda0080b3fd6b8aff1466557455a8429b143a
Miroslav Misik <mirom73@gmail.com>
2019-03-15 17:57:36 +0100

    finished ex05 in ch07, ex04 in ch09

---
+++ Chapter07/Exercise05/Exercise05.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Chapter07
+{
+    class Exercise05
+    {
+        static void Main(string[] args)
+        {
+            //5. Write a program, which finds the maximal sequence of consecutively 
+            //placed increasing integers.Example: { 3, 2, 3, 4, 2, 2, 4} -> { 2, 3, 4}. 
+            Console.WriteLine("EX05: Program, which finds the maximal sequence of consecutively\n" +
+                "      placed increasing integers. Example: { 3, 2, 3, 4, 2, 2, 4} -> { 2, 3, 4}.\n");
+
+            Console.Write("Enter numbers delimited by comma: ");
+            string[] inputIntArray = Console.ReadLine().Split(',');
+            int endIndex = 0;
+            int maxSequenceLength = 1;
+            int maxSequenceLengthTMP = 1;
+            for (int i = 1; i < inputIntArray.Length; i++)
+            {
+                if (int.Parse(inputIntArray[i - 1]) + 1 == int.Parse(inputIntArray[i]))
+                {
+                    maxSequenceLengthTMP += 1;
+                }
+                else
+                {
+                    if (maxSequenceLength < maxSequenceLengthTMP)
+                    {
+                        maxSequenceLength = maxSequenceLengthTMP;
+                        endIndex = i - 1;
+                        maxSequenceLengthTMP = 1;
+                    }
+                }
+            }
+            Console.WriteLine($"length = {maxSequenceLength}, start index = {endIndex - maxSequenceLength + 1}, " +
+                $"end index = {endIndex}");
+            Console.Write(inputIntArray[endIndex - maxSequenceLength + 1]);
+            for (int i = 1; i < maxSequenceLength; i++)
+            {
+                Console.Write($", {inputIntArray[endIndex - maxSequenceLength + 1 + i]}");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Chapter09/Exercise04/Exercise04.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter09
+{
+    class Exercise04
+    {
+        static void Main(string[] args)
+        {
+            //4. Write a method that finds how many times certain number can be
+            //found in a given array.Write a program to test that the method works correctly.
+            Console.WriteLine("EX04: Write a method that finds how many times certain number can be\n" +
+                "      found in a given array.Write a program to test that the method works correctly.\n");
+            Console.Write("Enter the array of the numbers delimited by the comma: ");
+            string[] inputArray = Console.ReadLine().Split(',');
+            Console.Write("Enter the number which count you are looking for: ");
+            string lookingFor = Console.ReadLine();
+
+            Console.WriteLine($"The number / string {lookingFor} is {CountElementInArray(inputArray, lookingFor)} " +
+                $"time/s in the array.");
+        }
+
+        static int CountElementInArray(string[] inputArray, string lookingFor)
+        {
+            int count = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (lookingFor == inputArray[i])
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+}
