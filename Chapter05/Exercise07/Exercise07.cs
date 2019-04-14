5eac8f38f64305ed9e0d5c1cccee399c8457b1ee
Miroslav Misik <mirom73@gmail.com>
2019-04-14 22:23:00 +0200

    chapter05 finished ex07

---
+++ Chapter05/Exercise07/Exercise07.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter05
+{
+    class Exercise07
+    {
+        static void Main(string[] args)
+        {
+            //7.Write a program that finds the greatest of given 5 numbers.
+            //Modified to find the largest number from the entered.
+            Console.WriteLine("EX07: The program that finds the greatest of given 5 numbers.\n" +
+                "Modified to find the largest number from the entered.\n");
+            Console.Write("Enter the numbers delimited by space:");            
+            List<int> enteredNumbersList = new List<int>();            
+            foreach (var item in Console.ReadLine().Split(new char[] { ' ' }))
+            {
+                enteredNumbersList.Add(int.Parse(item));
+            }
+            int[] enteredNumbers = enteredNumbersList.ToArray();
+            Array.Sort(enteredNumbers);
+            Console.WriteLine("The largest number is: " + enteredNumbers[enteredNumbers.Length - 1]);                       
+        }
+    }
+}
